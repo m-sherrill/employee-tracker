847a73359ce4f7ad5f96630c183c75b075959c1b
Morgan Sherrill <mo.sherrill@outlook.com>
2021-12-04 01:49:42 -0800

    started inquirer prompts

--- inquirer.js
+++ inquirer.js
@@ -1,2 +1,68 @@
-// inquirer constants 
-const inquirer = require('inquirer')
+// inquirer question constants
+
+// need to query the database to get options for department and managers
+
+const initialPrompt = [
+    {
+        name: "initialPrompt",
+        type: "list",
+        message: "What would you like to view first?",
+        choices: ["View All Departments", "View All Roles", "View all Employees", "Add a Department", "Add a Role", "Add an Employee", "Update a Role"],
+    }
+]
+
+
+const addDepartment = [
+    {
+        name: "departmentName",
+        type: "input",
+        message: "What is the department name?",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter a team name");
+            }
+            return true;
+        },
+    },
+]
+
+const addRole = [
+    {
+        name: "roleName",
+        type: "input",
+        message: "What is the name of this role?",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter a team name");
+            }
+            return true;
+        },
+    },
+    {
+        name: "roleSalary",
+        type: "input",
+        message: "What is the salary for this role?",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter a team name");
+            }
+            return true;
+        },
+    },
+    {
+        name: "roleDepartment",
+        type: "input",
+        message: "What department should this be assigned to?",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter a team name");
+            }
+            return true;
+        },
+    },
+]
+
+const addEmployee = [
+
+]
+
